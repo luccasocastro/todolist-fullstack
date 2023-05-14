59c27bcba52e2793d4ab836dc49c572a71a99767
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-05-14 20:55:12 -0300

    feat: criando rota para adicionar task

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -1,10 +1,23 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-    const tasks = await connection.execute('SELECT * FROM tasks');
+    const [tasks] = await connection.execute('SELECT * FROM tasks');
     return tasks;
 };
 
+const createTask = async (task) => {
+    const { title } = task;
+
+    const dateUTC = new Date(Date.now()).toUTCString();
+
+    const [createdTask] = await connection.execute(
+        'INSERT INTO tasks(title, status, created_at) VALUES (?, ?, ?)',
+         [title, 'pendente', dateUTC]);
+        
+    return {insertId: createdTask.insertId};
+};
+
 module.exports = {
-    getAll
+    getAll,
+    createTask,
 };
